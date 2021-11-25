699bafd4094b77f1e5a3169af425bb452d62a2f1
dodichakill <dodiherlangga91@gmail.com>
2021-11-25 22:45:20 +0700

    menambahkan sebuah data blog beserta assetnya dan memperbaiki beberapa tampilan

--- js/data-blog.js
+++ js/data-blog.js
@@ -1,42 +1,50 @@
 const dataBlog = [
   {
-    "id": 1,
-    "gambar": 'jagung.jpeg',
-    "judul": "mengenal jagung lebih dekat",
-    "deskripsi": "Jagung (Zea mays ssp. mays) adalah salah satu tanaman pangan penghasil karbohidrat yang terpenting di dunia, selain gandum dan padi. Bagi penduduk Amerika Tengah dan Selatan, bulir jagung adalah pangan pokok, sebagaimana bagi sebagian penduduk Afrika dan beberapa daerah di Indonesia. Pada masa kini, jagung juga sudah menjadi komponen penting pakan ternak. <br><br> Penggunaan lainnya adalah sebagai sumber minyak pangan dan bahan dasar tepung maizena. Berbagai produk turunan hasil jagung menjadi bahan baku berbagai produk industri farmasi, kosmetika, dan kimia. Diagram tanaman jagung. Jagung merupakan tanaman model yang menarik,[1][2] khususnya di bidang biologi dan pertanian. Sejak awal abad ke-20, tanaman ini menjadi objek penelitian genetika yang intensif, dan membantu terbentuknya teknologi kultivar hibrida yang revolusioner. Dari sisi fisiologi, tanaman ini tergolong tanaman C4 sehingga sangat efisien memanfaatkan sinar matahari. Dalam kajian agronomi, tanggapan jagung yang dramatis dan khas terhadap kekurangan atau keracunan unsur-unsur hara penting menjadikan jagung sebagai tanaman percobaan fisiologi pemupukan yang disukai'",
-    "tanggal": '12-04-2021',
-    "sumber": "https://id.wikipedia.org/wiki/Jagung"
+    id: 1,
+    gambar: 'utama.jpeg',
+    judul: "Menu Makanan 4 Sehat 5 Sempurna",
+    deskripsi: "Sejak kita kecil, pasti sudah diajarkan mengenai 4 sehat 5 sempurna untuk asupan sehari-hari agar tubuh terus sehat. Nah, ajaran 4 sehat 5 sempurna juga banyak diterapkan kepada generasi selanjutnya hingga pada saat kita memiliki buah hati sendiri untuk dirawat.<br><br>Salah satu cara yang baik dan sehat untuk merawat buah hati adalah dengan asupan yang tepat dengan 4 sehat 5 sempurna ini. Jadi, sebenarnya apa sih 4 sehat 5 sempurna itu?<br><br>Slogan ini adalah kampanye yang dilakukan pemerintah sejak tahun 1955 untuk membuat masyarakat memahami pola makan yang benar. Makanan pokok sehat terdapat karbohidrat seperti nasi atau kentang, lalu ada lauk pauk, buah dan sayuran, serta dilengkapi susu untuk menyempurnakannya.<br><br>Namun, makanan 4 sehat 5 sempurna yang itu-itu saja dapat membuat si kecil bosan dan akhirnya meminta asupan yang lain dan lebih bervariasi. Apa saja variasi tersebut? Yuk, simak di bawah ini!<br><h3>Menu Makanan 4 sehat 5 sempurna</h3><ol><li><span>Makanan Pokok (Satu Sehat)</span><p>Makanan pokok adalah makanan yang dibutuhkan tubuh sebagai sumber energi sehari-hari yang kaya akan karbohidrat. Makanan pokok ini memberikan sifat ketergantungan untuk manusia.<br>Contohnya, pasti kamu akan tetap merasa lapar jika belum mengonsumsi makanan pokok seperti nasi. Maka dari itu, makanan pokok tergolong ke dalam salah satu keperluan pokok manusia.<br>Nah, untuk memberikan variasi makanan pokok pada anak, cobalah gunakan beberapa makanan pokok berikut: Beras, Jagung, Gandum, Kentang,Oat dan Umbi-umbian</p></li><li><span>Lauk Pauk (Dua Sehat)</span><p>Lauk pauk adalah pendamping makanan pokok yang berfungsi untuk sumber zat pembangun tubuh. Lauk pauk biasanya memiliki kandungan protein yang berguna sebagai pembangun dan pengatur bagi tubuh.<br>Selain itu, protein juga dapat digunakan untuk sumber cadangan energi dan sistem kekebalan tubuh.<br>Untuk variasi makanan agar tidak membosankan, kamu dapat menggunakan lauk pauk berikut: Ikan, daging ayam, daging sapi, daging kambing, daging bebek, telur, tempe dan tahu</p></li><li><span>Sayur-sayuran dan Buah-buahan (Tiga & Empat Sehat)</span><p>Sayuran dan buah-buahan adalah asupan yang baik bagi kesehatan tubuh untuk pertumbuhan dan perkembangan serta dapat menjaga ketahanan tubuh.<br>Kedua buah-buahan dan sayuran memiliki banyak jenis yang memberikan fungsi dan nutrisi nya masing-masing. Contohnya, sayuran yang baik adalah berwarna hijau dengan kandungan vitamin, serat dan protein nabati.</p></li><li><span>Susu (Lima Sempurna)</span><p>Beruntung untuk kamu yang menyukai minum susu. Susu ini berasal dari sumber protein hewani yang baik untuk pembentukan dan pertumbuhan tulang, dan juga meningkatkan energi.<br>Selain susu dari protein hewani, juga ada susu untuk kamu para vegan yang terbuat dari protein nabati yakni susu kedelai</p></li></ol>",
+    tanggal: '17-04-2021',
+    sumber: "https://www.tokopedia.com/blog/menu-makanan-4-sehat-5-sempurna-hlt/"
   },
   {
-    "id": 2,
-    "gambar": 'daging.jpeg',
-    "judul": 'ketahui lebih dalam tentang daging ',
-    "deskripsi": 'Daging ialah bagian lunak pada hewan yang terbungkus kulit dan melekat pada tulang yang menjadi bahan makanan. Daging tersusun sebagian besar dari jaringan otot, ditambah dengan lemak yang melekat padanya, urat, serta tulang rawan.Sebagai komoditas dagang, daging biasanya disematkan untuk yang berasal dari hewan besar (mamalia dan reptil) saja. Daging semacam ini disebut pula "daging merah", dan diperdagangkan dalam bentuk potongan-potongan, Sementara itu ikan, amfibi, hewan laut dan unggas tidak termasuk komoditas daging, karena dapat diperdagangkan secara utuh. Daging non-komoditas disebut pula "daging putih" Domestikasi hewan, terbukti bermula sejak zaman glasial terakhir (sekitar 10.000 SM),[1]:2 memungkinkan produksi daging secara sistematis dan pengembangbiakan hewan dengan maksud untuk meningkatkan produksi daging.[1]:2 Hewan yang sekarang menjadi sumber utama daging dijinakkan bersamaan dengan perkembangan peradaban awal ',
-    "tanggal": '10-04-2021',
-    "sumber": "https://id.wikipedia.org/wiki/Daging"
+    id: 2,
+    gambar: 'jagung.jpeg',
+    judul: "mengenal jagung lebih dekat",
+    deskripsi: "Jagung (Zea mays ssp. mays) adalah salah satu tanaman pangan penghasil karbohidrat yang terpenting di dunia, selain gandum dan padi. Bagi penduduk Amerika Tengah dan Selatan, bulir jagung adalah pangan pokok, sebagaimana bagi sebagian penduduk Afrika dan beberapa daerah di Indonesia Pada masa kini, jagung juga sudah menjadi komponen penting pakan ternak. <br><br> Penggunaan lainnya adalah sebagai sumber minyak pangan dan bahan dasar tepung maizena. Berbagai produk turunan hasil jagung menjadi bahan baku berbagai produk industri farmasi, kosmetika, dan kimia. Diagram tanaman jagung. Jagung merupakan tanaman model yang menarik,[1][2] khususnya di bidang biologi dan pertanian. <br><br> Sejak awal abad ke-20, tanaman ini menjadi objek penelitian genetika yang intensif, dan membantu terbentuknya teknologi kultivar hibrida yang revolusioner. Dari sisi fisiologi, tanaman ini tergolong tanaman C4 sehingga sangat efisien memanfaatkan sinar matahari. Dalam kajian agronomi, tanggapan jagung yang dramatis dan khas terhadap kekurangan atau keracunan unsur-unsur hara penting menjadikan jagung sebagai tanaman percobaan fisiologi pemupukan yang disukai'",
+    tanggal: '12-04-2021',
+    sumber: "https://id.wikipedia.org/wiki/Jagung"
   },
   {
-    "id": 3,
-    "gambar": 'gandum.jpeg',
-    "judul": 'ketahui fakta tentang gandum',
-    "deskripsi": 'Gandum (Triticum spp.) adalah sekelompok tanaman serealia dari suku padi-padian yang kaya akan karbohidrat. Gandum biasanya digunakan untuk memproduksi tepung terigu, pakan ternak, ataupun difermentasi untuk menghasilkan alkohol. Pada umumnya, biji gandum (kernel) berbentuk opal dengan panjang 6–8 mm dan diameter  2–3 mm. Seperti jenis serealia lainnya, gandum memiliki tekstur yang keras. Biji gandum terdiri dari tiga bagian yaitu bagian kulit (bran), bagian endosperma, dan bagian lembaga (germ). Masyarakat prasejarah sudah mengenal sifat-sifat gandum dan tanaman biji-bijian lainnya sebagai sumber makanan. Berdasarkan penggalian arkeolog, diperkirakan gandum berasal dari daerah sekitar Laut Merah dan Laut Mediterania, yaitu daerah sekitar Turki, Siria, Irak, dan Iran. Sejarah Tiongkok menunjukkan bahwa budidaya gandum telah ada sejak 2700 SM',
-    "tanggal": '09-04-2021',
-    "sumber": 'https://id.wikipedia.org/wiki/Gandum'
+    id: 3,
+    gambar: 'daging.jpeg',
+    judul: 'ketahui lebih dalam tentang daging ',
+    deskripsi: 'Daging ialah bagian lunak pada hewan yang terbungkus kulit dan melekat pada tulang yang menjadi bahan makanan. Daging tersusun sebagian besar dari jaringan otot, ditambah dengan lemak yang melekat padanya, urat, serta tulang rawan. <br><br> Sebagai komoditas dagang, daging biasanya disematkan untuk yang berasal dari hewan besar (mamalia dan reptil) saja. Daging semacam ini disebut pula "daging merah", dan diperdagangkan dalam bentuk potongan-potongan, Sementara itu ikan, amfibi, hewan laut dan unggas tidak termasuk komoditas daging, karena dapat diperdagangkan secara utuh. <br><br>Daging non-komoditas disebut pula "daging putih" Domestikasi hewan, terbukti bermula sejak zaman glasial terakhir (sekitar 10.000 SM),[1]:2 memungkinkan produksi daging secara sistematis dan pengembangbiakan hewan dengan maksud untuk meningkatkan produksi daging.[1]:2 Hewan yang sekarang menjadi sumber utama daging dijinakkan bersamaan dengan perkembangan peradaban awal ',
+    tanggal: '10-04-2021',
+    sumber: "https://id.wikipedia.org/wiki/Daging"
   },
   {
-    "id": 4,
-    "gambar": 'kentang.jpeg',
-    "judul": 'informasi seputar kentang',
-    "deskripsi": 'Menurut sejarahnya, kentang berasal dari lembah-lembang dataran tinggi di Chili, Peru, dan Meksiko. Jenis tersebut diperkenalkan bangsa Spanyol dari Peru ke Eropa sejak tahun 1565. Semenjak itulah, kentang menyebar ke negara-negara lain -termasuk Indonesia-. Menurut catatan awal di Indonesia, tumbuhan ini mulai ada semenjak tahun 1794, dimulai dengan penanaman di sekitar Cimahi.[1] Semenjak itu, kentang dapat ditemui pula di Priangan dan Gunung Tengger. Pada tahun 1812, kentang sudah dikenal dan dijual di Kedu. Sedangkan, di Sumatra tumbuhan ini dikenal setahun sebelumnya, 1811. Kentang tumbuh di pegunungan dengan ketinggian antara 1000 mdpl hingga 2000 mdpl, pada tanah humus. Tanah bekas letusan gunung berapi yang berstruktur remah lebih disukai. Jagung budidaya dianggap sebagai keturunan langsung sejenis tanaman rerumputan mirip jagung yang bernama teosinte (Zea mays ssp. parviglumis). Dalam proses domestikasinya, yang berlangsung paling tidak 7 000 tahun lalu oleh penduduk asli setempat, masuk gen-gen dari subspesies lain, terutama Zea mays ssp. mexicana. Istilah teosinte sebenarnya digunakan untuk menggambarkan semua spesies dalam genus Zea, kecuali Zea mays ssp. mays. Proses domestikasi menjadikan jagung merupakan satu-satunya spesies tumbuhan yang tidak dapat hidup secara liar di alam',
-    "tanggal": '07-04-2021',
-    "sumber": 'https://id.wikipedia.org/wiki/Kentang'
+    id: 4,
+    gambar: 'gandum.jpeg',
+    judul: 'ketahui fakta tentang gandum',
+    deskripsi: 'Gandum (Triticum spp.) adalah sekelompok tanaman serealia dari suku padi-padian yang kaya akan karbohidrat. Gandum biasanya digunakan untuk memproduksi tepung terigu, pakan ternak, ataupun difermentasi untuk menghasilkan alkohol. <br><br>Pada umumnya, biji gandum (kernel) berbentuk opal dengan panjang 6–8 mm dan diameter  2–3 mm. Seperti jenis serealia lainnya, gandum memiliki tekstur yang keras. Biji gandum terdiri dari tiga bagian yaitu bagian kulit (bran), bagian endosperma, dan bagian lembaga (germ). Masyarakat prasejarah sudah mengenal sifat-sifat gandum dan tanaman biji-bijian lainnya sebagai sumber makanan. <br><br>Berdasarkan penggalian arkeolog, diperkirakan gandum berasal dari daerah sekitar Laut Merah dan Laut Mediterania, yaitu daerah sekitar Turki, Siria, Irak, dan Iran. Sejarah Tiongkok menunjukkan bahwa budidaya gandum telah ada sejak 2700 SM',
+    tanggal: '09-04-2021',
+    sumber: 'https://id.wikipedia.org/wiki/Gandum'
   },
   {
-    "id": 5,
-    "gambar": 'telur.jpeg',
-    "judul": 'mengetahui lebih dalam tentang telur',
-    "deskripsi": 'Telur adalah salah satu bahan makanan hewani yang dikonsumsi selain daging, ikan dan susu. Umumnya telur yang dikonsumsi berasal dari jenis-jenis burung, seperti ayam, bebek, dan angsa, akan tetapi telur-telur yang lebih kecil seperti telur ikan kadang juga digunakan sebagai campuran dalam hidangan. Selain itu dikonsumsi pula juga telur yang berukuran besar seperti telur burung unta ataupun sedang, misalnya telur penyu. Sebagian besar produk telur ayam ditujukan untuk dikonsumsi orang tidak disterilkan, mengingat ayam petelur yang menghasilkannya tidak didampingi oleh ayam pejantan. Telur yang disterilkan dapat pula dipesan dan dimakan sebagaimana telur-telur yang tidak disterilkan, dengan sedikit perbedaan kandungan nutrisi. Telur yang disterilkan tidak akan mengandung embrio yang telah berkembang, sebagaimana lemari pendingin mencegah pertumbuhan sel-sel dalam telur',
-    "tanggal": '06-04-2021',
-    "sumber": 'https://id.wikipedia.org/wiki/Telur_(makanan)'
+    id: 5,
+    gambar: 'kentang.jpeg',
+    judul: 'informasi seputar kentang',
+    deskripsi: 'Kentang (Solanum tuberosum) adalah tanaman dari suku Solanaceae yang memiliki umbi batang yang dapat dimakan dan disebut "kentang". Umbi kentang sekarang telah menjadi salah satu makanan pokok penting di Eropa walaupun pada awalnya didatangkan dari Amerika Selatan.<br><br>Penjelajah Spanyol dan Portugis pertama kali membawa ke Eropa dan mengembangbiakkan tanaman ini.Tanaman kentang asalnya dari Amerika Selatan dan telah dibudidayakan oleh penduduk di sana sejak ribuan tahun silam. Tanaman ini merupakan herba (tanaman pendek tidak berkayu) semusim dan menyukai iklim yang sejuk. <br><br> Di daerah tropis cocok ditanam di dataran tinggi.Bunga sempurna dan tersusun majemuk. Ukuran cukup besar, dengan diameter sekitar 3cm. Warnanya berkisar dari ungu hingga putih',
+    tanggal: '07-04-2021',
+    sumber: 'https://id.wikipedia.org/wiki/Kentang'
+  },
+  {
+    id: 6,
+    gambar: 'telur.jpeg',
+    judul: 'mengetahui lebih dalam tentang telur',
+    deskripsi: 'Telur adalah salah satu bahan makanan hewani yang dikonsumsi selain daging, ikan dan susu. Umumnya telur yang dikonsumsi berasal dari jenis-jenis burung, seperti ayam, bebek, dan angsa, akan tetapi telur-telur yang lebih kecil seperti telur ikan kadang juga digunakan sebagai campuran dalam hidangan.<br><br> Selain itu dikonsumsi pula juga telur yang berukuran besar seperti telur burung unta ataupun sedang, misalnya telur penyu. Sebagian besar produk telur ayam ditujukan untuk dikonsumsi orang tidak disterilkan, mengingat ayam petelur yang menghasilkannya tidak didampingi oleh ayam pejantan. <br><br>Telur yang disterilkan dapat pula dipesan dan dimakan sebagaimana telur-telur yang tidak disterilkan, dengan sedikit perbedaan kandungan nutrisi. Telur yang disterilkan tidak akan mengandung embrio yang telah berkembang, sebagaimana lemari pendingin mencegah pertumbuhan sel-sel dalam telur',
+    tanggal: '06-04-2021',
+    sumber: 'https://id.wikipedia.org/wiki/Telur_(makanan)'
   },
 ]
